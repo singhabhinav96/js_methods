98af50f42212bf8a97895f3dc15790ed5f1d4780
Abhinav <singh.abhinav6996@gmail.com>
2019-07-12 12:53:15 +0530

    exercise

--- array-methods/2-array.js
+++ array-methods/2-array.js
@@ -17,11 +17,32 @@ console.log(max);
 
 var numbers1 = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 // Create a sumArray function that takes an array of numbers1 as a parameter, and calculate the sum of all its numbers
+var sum = 0;
+function sumArray(numbers1) {
+  for (let i = 0; i < numbers1.length; i++) {
+    sum += numbers1[i];
+  }
+  return sum;
+}
+sumArray(numbers1);
 // Use reduce method of array
+var sum = numbers1.reduce(function(acc, curr) {
+  return acc + curr;
+});
 // Use the above sum and calculate the average.
+var avg = sum / numbers1.length;
 
 var numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 //Write a function averageNumbers that receives an array of numbers2 and calculate the average of the numbers
+var sum = 0;
+function averageNumbers(numbers2) {
+  for (let i = 0; i < numbers2.length; i++) {
+    sum += numbers2[i];
+    avg = sum / numbers2.length;
+  }
+  return avg;
+}
+averageNumbers(numbers2);
 
 var words2 = [
   "seat",
--- array-methods/5-object.js
+++ array-methods/5-object.js
@@ -1,15 +1,13 @@
 // 1. Write a JavaScript program to list the properties and values of a JavaScript object. (Object.keys)
-
-
+for (let [key, value] of Object.entries(student)) {
+  console.log(`${key} ${value}`);
+}
 
 // 2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
-var student = { 
-  name : "David Rayy", 
-  sclass : "VI", 
-  rollno : 12
+var student = {
+  name: "David Rayy",
+  sclass: "VI",
+  rollno: 12
 };
-
-
-
 
 // 3. Write a function to get the length of an object.
